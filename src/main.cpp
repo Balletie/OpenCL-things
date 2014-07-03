b039faaf26449ccec152fe3fc64366f8ce3211f9
Balletie <skip_meesie@hotmail.com>
2014-07-03 15:04:18 +0200

    Print all of the devices

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,24 @@
 
 int main() {
 	cl_int err = 0;
+	cl_uint num_devices;
 	cl_platform_id plat;
-	cl_device_id dev;
 
 	err = clGetPlatformIDs(1, &plat, NULL);
-	if (err != CL_SUCCESS) printf("ERROR");
-	err = clGetDeviceIDs(plat, CL_DEVICE_TYPE_ALL, 1, &dev, NULL);
-	if (err != CL_SUCCESS) printf("ERROR");
+	if (err != CL_SUCCESS) printf("ERROR at line %u", __LINE__);
+
+	err = clGetDeviceIDs(plat, CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices);
+	if (err != CL_SUCCESS) printf("ERROR at line %u", __LINE__);
+	printf("There are %u devices found, namely:\n", num_devices);
+
+	cl_device_id devices[num_devices];
+
+	err = clGetDeviceIDs(plat, CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
+	if (err != CL_SUCCESS) printf("ERROR at line %u", __LINE__);
 	
-	char info[1024];
-	clGetDeviceInfo(dev, CL_DEVICE_NAME, 1024, info, NULL);
-	printf("Using OpenCL device %s\n", info);
+	char info[num_devices][1024];
+	for (int i = 0; i < num_devices; i++) {
+		clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 1024, &info[i], NULL);
+		printf(" * %s\n", info[i]);
+	}
 }
