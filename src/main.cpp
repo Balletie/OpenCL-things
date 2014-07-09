e49a54d857b97b6a7065798bcb8a00afcd463ffc
Balletie <skip_meesie@hotmail.com>
2014-07-09 15:45:05 +0200

    Rename the files (both are C++ files now)

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,146 @@
-#define __CL_ENABLE_EXCEPTIONS
-#include <OpenCL/cl.hpp>
+#ifdef __APPLE__
+  #include <OpenCL/opencl.h>
+#else
+  #include <CL/cl.h>
+#endif
+#include "file.h"
+#include <ios>
 #include <stdio.h>
-#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+void printCLDeviceInfo(cl_device_id device, bool print_extensions) {
+	char name[256];
+	clGetDeviceInfo(device, CL_DEVICE_NAME, 256, name, NULL);
+	printf(" * %s\n", name);
+	if (!print_extensions) return;
+
+	char info[1024];
+	printf("    Extensions:\n     ");
+	clGetDeviceInfo(device, CL_DEVICE_EXTENSIONS, 1024, info, NULL);
+	for (int j = 0; j < 1024; j++) {
+		if (info[j] == 0x00) break;
+		if (info[j] == 0x20) {
+			putchar('\n');
+			if (info[j+1] == 0x00) continue;
+			for (int k = 0; k < 5; k++) putchar(' ');
+			continue;
+		}
+		putchar(info[j]);
+	}
+}
 
 int main() {
-	try {
-		std::vector<cl::Platform> platforms;
-		cl::Platform::get(&platforms);
-		if (platforms.size() > 1) 	printf("There are %u platforms:\n", platforms.size());
-		else if (platforms.size() == 1) printf("There's exactly one platform:\n");
-		else 				printf("There are no available platforms :("), exit(0);
-		for (int i = 0; i < platforms.size(); i++) {
-			std::string plat_info;
-			std::vector<cl::Device> devices;
-			platforms[i].getInfo(CL_PLATFORM_VERSION, &plat_info);
-			printf("Platform %d:\n", i);
-			printf(" * %s\n", plat_info.c_str());
-			platforms[i].getDevices(CL_DEVICE_TYPE_ALL, &devices);
-			printf("There are %u devices found:\n", devices.size());
-			for (int j = 0; j < devices.size(); j++) {
-				std::string device_name;
-				std::string device_info;
-				devices[i].getInfo(CL_DEVICE_NAME, &device_name);
-				devices[i].getInfo(CL_DEVICE_EXTENSIONS, &device_info);
-				printf(" * %s\n    Extensions:\n     ", device_name.c_str());
-				const char* device_info_c_str = device_info.c_str();
-				for (int k = 0; k < device_info.size(); k++) {
-					if (device_info_c_str[k] == 0x00) break;
-					if (device_info_c_str[k] == 0x20) {
-						putchar('\n');
-						if (device_info_c_str[k+1] == 0x00) continue;
-						for (int l = 0; l < 5; l++) putchar(' ');
-						continue;
-					}
-					putchar(device_info_c_str[k]);
-				}
-				putchar('\n');
+	std::ios::sync_with_stdio();
+	cl_int err = 0;
+	cl_uint num_devices;
+	cl_uint num_plats;
+	cl_platform_id *plat;
+	cl_device_id final_device;
+
+	err = clGetPlatformIDs(0, NULL, &num_plats);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+	if (num_plats > 1)		printf("There are %u platforms\n", num_plats);
+	else if (num_plats == 1)	printf("There's exactly one platform, namely:\n");
+	else				printf("There are no available platforms"), exit(0);
+
+	plat = (cl_platform_id*) malloc(sizeof(cl_platform_id) * num_plats);
+	err = clGetPlatformIDs(num_plats, plat, NULL);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+
+	for (int i = 0; i < num_plats; i++) {
+		char plat_info[1024];
+
+		err = clGetPlatformInfo(plat[i], CL_PLATFORM_VERSION, 1024, plat_info, NULL);
+		if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+		printf("Platform %d:\n", i);
+		printf(" * %s\n", plat_info);
+
+		err = clGetDeviceIDs(plat[i], CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices);
+		if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+		printf("There are %u devices found, namely:\n", num_devices);
+
+		cl_device_id devices[num_devices];
+		err = clGetDeviceIDs(plat[i], CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
+		if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+		
+		for (int j = 0; j < num_devices; j++) {
+			cl_device_type current_type;
+			clGetDeviceInfo(devices[j], CL_DEVICE_TYPE, sizeof(cl_device_type), &current_type, NULL);
+			if (current_type == CL_DEVICE_TYPE_GPU) {
+				printf("Found GPU\n");
+				final_device = devices[j];
 			}
+
+			printCLDeviceInfo(devices[j],false);
+			if (j != num_devices - 1) putchar('\n');
 		}
-	} catch (cl::Error err) {
-		std::cerr << "ERROR: " << err.what() << "(" << err.err() << ")" << std::endl;
 	}
+	printf("\n");
+
+	unsigned int count = 1024;
+	size_t local;
+	size_t global = count;
+	float input_data[count];
+	// Fill the array
+	srand(time(NULL));
+	for (int i = 0; i < count; i++) input_data[i] = rand() / (float)RAND_MAX;	
+	float output_data[count];
+	float results[count];
+	const char* program_source = readFile("kernel/square.cl");
+	printf("Loaded kernel program source:\n%s\n", program_source);
+
+	// Set OpenCL context
+	cl_context context = clCreateContext(0, 1, &final_device, NULL, NULL, &err);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u: Failed to make OpenCL context\n", __LINE__);
+
+	// Create command queue
+	cl_command_queue commands = clCreateCommandQueue(context, final_device, 0, &err);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+
+	// Create the program from source and build it.
+	cl_program program = clCreateProgramWithSource(context, 1, (const char **) &program_source, NULL, &err);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+	err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+	cl_kernel kernel = clCreateKernel(program, "square", &err);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+
+	// Create the buffers for input and output arrays.
+	cl_mem read_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float)*count, NULL, &err);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+	cl_mem write_buffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*count, NULL, &err);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+
+	// Copy the input data (blocking)
+	err = clEnqueueWriteBuffer(commands, read_buffer, CL_TRUE, 0, sizeof(float)*count, input_data, 0, NULL, NULL);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+
+	// Set the kernel arguments
+	err = 0;
+	err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &read_buffer);
+	err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &write_buffer);
+	err |= clSetKernelArg(kernel, 2, sizeof(unsigned int), &count);	
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+
+	// Get the maximum work group size
+	err = clGetKernelWorkGroupInfo(kernel, final_device, CL_KERNEL_WORK_GROUP_SIZE, sizeof(local), &local, NULL);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+
+	// Execute the kernel
+	err = clEnqueueNDRangeKernel(commands, kernel, 1, NULL, &global, &local, 0, NULL, NULL);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+
+	// Give it time to finish
+	clFinish(commands);
+
+	err = clEnqueueReadBuffer(commands, write_buffer, CL_TRUE, 0, sizeof(float)*count, output_data, 0, NULL, NULL);
+	if (err != CL_SUCCESS)		printf("ERROR at line %u\n", __LINE__);
+
+	// Validate everything
+	int correct = 0;
+	for (int i = 0; i < count; i++) {
+		if (output_data[i] == input_data[i]*input_data[i]) correct++;
+	}
+	printf("%d/%d correct results\n", correct, count);
 }
---
+++ src/main_alternative.cpp
@@ -0,0 +1,44 @@
+#define __CL_ENABLE_EXCEPTIONS
+#include <OpenCL/cl.hpp>
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+	try {
+		std::vector<cl::Platform> platforms;
+		cl::Platform::get(&platforms);
+		if (platforms.size() > 1) 	printf("There are %u platforms:\n", platforms.size());
+		else if (platforms.size() == 1) printf("There's exactly one platform:\n");
+		else 				printf("There are no available platforms :("), exit(0);
+		for (int i = 0; i < platforms.size(); i++) {
+			std::string plat_info;
+			std::vector<cl::Device> devices;
+			platforms[i].getInfo(CL_PLATFORM_VERSION, &plat_info);
+			printf("Platform %d:\n", i);
+			printf(" * %s\n", plat_info.c_str());
+			platforms[i].getDevices(CL_DEVICE_TYPE_ALL, &devices);
+			printf("There are %u devices found:\n", devices.size());
+			for (int j = 0; j < devices.size(); j++) {
+				std::string device_name;
+				std::string device_info;
+				devices[i].getInfo(CL_DEVICE_NAME, &device_name);
+				devices[i].getInfo(CL_DEVICE_EXTENSIONS, &device_info);
+				printf(" * %s\n    Extensions:\n     ", device_name.c_str());
+				const char* device_info_c_str = device_info.c_str();
+				for (int k = 0; k < device_info.size(); k++) {
+					if (device_info_c_str[k] == 0x00) break;
+					if (device_info_c_str[k] == 0x20) {
+						putchar('\n');
+						if (device_info_c_str[k+1] == 0x00) continue;
+						for (int l = 0; l < 5; l++) putchar(' ');
+						continue;
+					}
+					putchar(device_info_c_str[k]);
+				}
+				putchar('\n');
+			}
+		}
+	} catch (cl::Error err) {
+		std::cerr << "ERROR: " << err.what() << "(" << err.err() << ")" << std::endl;
+	}
+}
